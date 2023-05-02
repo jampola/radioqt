1fb8a4a0c6d4e8bb6f18b5c4c01afb5754e1e665
James Bos <james.bos@willdooit.com>
2023-05-02 23:04:30 +1000

    Build bin, add missing lib, update gitignore

---
+++ lib/icecast_stream_list.py
@@ -0,0 +1,71 @@
+from pyradios import RadioBrowser
+from logging import getLogger
+
+_logger = getLogger(__name__)
+
+rb = RadioBrowser()
+
+"""Advanced search.
+
+It will search for the station whose attribute
+contains the search term.
+
+Args:
+    name (str, optional): Name of the station.
+    name_exact (bool, optional): Only exact matches, otherwise all
+        matches (default: False).
+    country (str, optional): Country of the station.
+    country_exact (bool, optional): Only exact matches, otherwise
+        all matches (default: False).
+    countrycode (str, optional): 2-digit countrycode of the station
+        (see ISO 3166-1 alpha-2)
+    state (str, optional): State of the station.
+    state_exact (bool, optional): Only exact matches, otherwise all
+        matches. (default: False)
+    language (str, optional): Language of the station.
+    language_exact (bool, optional): Only exact matches, otherwise
+        all matches. (default: False)
+    tag (str, optional): Tag of the station.
+    tag_exact (bool, optional): Only exact matches, otherwise all
+        matches. (default: False)
+    tag_list (str, optional): A comma-separated list of tag.
+    bitrate_min (int, optional): Minimum of kbps for bitrate field of
+        stations in result. (default: 0)
+    bitrate_max (int, optional): Maximum of kbps for bitrate field of
+        stations in result. (default: 1000000)
+    order (str, optional): The result list will be sorted by: name,
+        url, homepage, favicon, tags, country, state, language, votes,
+        codec, bitrate, lastcheckok, lastchecktime, clicktimestamp,
+        clickcount, clicktrend, random
+    reverse (bool, optional): Reverse the result list if set to true.
+        (default: false)
+    offset (int, optional): Starting value of the result list from
+        the database. For example, if you want to do paging on the
+        server side. (default: 0)
+    limit (int, optional): Number of returned datarows (stations)
+        starting with offset (default 100000)
+    hidebroken (bool, optional): do list/not list broken stations.
+        Note: Not documented in the "Advanced Station Search".
+
+Returns:
+    list: Stations.
+
+Example:
+    >>> from pyradios import RadioBrowser
+    >>> rb = RadioBrowser()
+    >>> rb.search(name='BBC Radio 1', name_exact=True)
+
+See details:
+    https://de1.api.radio-browser.info/#Advanced_station_search
+"""
+
+def get_stream_list(text, search_option):
+    try:
+        if search_option == "Name":
+            return rb.search(name=text, hidebroken=True)
+        elif search_option == "Tag":
+            return rb.search(tag=text, hidebroken=True)
+        elif search_option == "Country":
+            return rb.search(country=text, hidebroken=True)
+    except Exception as e:
+        _logger.error(f"error: {e}")
